b64c0d36c4c804ab9205a5a8c41c074a98a3e70b
raisa khaliulina <evadar31@gmail.com>
2022-02-10 02:29:21 +0300

    Middleware. Logger

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,15 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore, compose } from "redux";
 import  taskReducer from "./task";
+import { logger } from "./moddeware/logger";
 
+const middlewareEnhancer = applyMiddleware(logger)
 
-
- function configureStore() {
-  return createStore(taskReducer);
+function configureStore() {
+  return createStore(
+    taskReducer, compose(
+       middlewareEnhancer, 
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__())
+  );
 }
 export default configureStore
